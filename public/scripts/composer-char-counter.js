e9d19f0a84fdcabf51dd2cffa605c5a25e999cae
Steven Xu <s@stevenxu.xyz>
2017-06-15 13:20:33 +0000

    update char count on new-tweet submit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,12 @@
 var MAX_TWEET_LENGTH = 140;
+
+
 $(document).ready(function () {
-  // use keypress for keystrokes
-  // include keyup for pasting
-  $('.new-tweet textarea').on('input', function () {
-    var that = $(this);
-    var text = that.val();
-    var counter = that.closest('.new-tweet').find('.counter');
+  
+  function updateCounter() {
+    var textarea = $('.new-tweet textarea');
+    var text = textarea.val();
+    var counter = textarea.closest('.new-tweet').find('.counter');
     var charsLeft = MAX_TWEET_LENGTH - text.length;
     
     counter.text(charsLeft);
@@ -14,5 +15,9 @@ $(document).ready(function () {
     } else {
       counter.removeClass("invalid");
     }
-  });
+  }
+  // use keypress for keystrokes
+  // include keyup for pasting
+  $('.new-tweet textarea').on('input', updateCounter);
+  $('.new-tweet form').on('submit', updateCounter);
 });
